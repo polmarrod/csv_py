420874fe753ee1ccb5f90c837a7c7bb9219faf17
pol <pol.martinez.rodriguez.7e6@itb.cat>
2023-10-04 17:24:25 +0200

    tot fet

---
+++ 1.py
@@ -0,0 +1,107 @@
+import csv
+
+class constants ():
+    PATHFITXERO = "basket_players.csv"
+    PATHNEWFITXERO = "jugadors_basket.csv"
+    FOOT = 2.54
+    POUND = 0.45
+    SEPARADOROLD= ";"
+    SEPARADORNEW = "^"
+    NAME = "names"
+    TEAM = "team"
+    POSITION = "position"
+    HEIGHT = "height"
+    WEIGHT = "weight"
+    AGE = "age"
+    NEWHEADERS = ["Nom", "Equip", "Posicio", "Altura", "Pes", "Edat"]
+
+def showLines(reader : enumerate):
+    for index, row in reader:
+        print(",".join(row) , end=" ")
+        print("Row number:" + str(index))
+def toDictionarie(reader : enumerate):
+    dictionarie = {
+        constants.NAME : [],
+        constants.TEAM : [],
+        constants.POSITION : [],
+        constants.HEIGHT : [],
+        constants.WEIGHT : [],
+        constants.AGE : []
+    }
+    
+    for index, row in reader:
+        if index != 0:
+            dictionarie[constants.NAME].append(row[0])     
+            dictionarie[constants.TEAM].append(row[1])  
+            dictionarie[constants.POSITION].append(row[2])     
+            dictionarie[constants.HEIGHT].append(row[3])  
+            dictionarie[constants.WEIGHT].append(row[4])  
+            dictionarie[constants.AGE].append(row[5])  
+    return dictionarie
+def changePosition(book : dict[str, list]):
+    index = 0
+    cons = constants.POSITION
+    for index, position in enumerate(book[cons]):
+        match position:
+            case "Point Guard":
+                book[cons][index] = "Base"
+            case "Shooting Guard":
+                book[cons][index] = "Escorta"
+            case "Small Forward":
+                book[cons][index] = "Aler"
+            case "Power Forward":
+                book[cons][index] = "Ala-pivot"
+            case "Center":
+                book[cons][index] = "Pivot"
+            case other : 
+                ""
+        index += 1
+    return book      
+def changeHeightOrWeight(book : dict[str, list], parameter : str):
+    value = 0.0
+    if parameter == constants.HEIGHT:
+        value = constants.FOOT
+    elif parameter == constants.WEIGHT : 
+        value = constants.POUND
+    else : 
+        return
+    for index, thing in enumerate(book[parameter]):
+        book[parameter][index] = round( round(float(thing), 2) * value, 2)
+        index += 1
+    return book
+
+def showParameter (book : dict[str, list], parameter: str):
+    for thing in book[parameter]:
+        print(thing)
+
+def changeAge(book : dict[str, list] , decimals : int):
+    for index, age in enumerate(book[constants.AGE]):
+        book[constants.AGE][index] = int(round(float(age), decimals))
+    return book
+
+def newCSVFile(book : dict[str, list]):    
+    with open(constants.PATHNEWFITXERO, mode= "w") as file:
+        writer = csv.DictWriter(file, delimiter=constants.SEPARADORNEW, fieldnames= constants.NEWHEADERS)
+        writer.writeheader()
+        for index in range(len(book[constants.NAME])):
+            row = {
+                constants.NEWHEADERS[0] : book[constants.NAME][index], 
+                constants.NEWHEADERS[1] : book[constants.TEAM][index], 
+                constants.NEWHEADERS[2] : book[constants.POSITION][index], 
+                constants.NEWHEADERS[3] : book[constants.HEIGHT][index], 
+                constants.NEWHEADERS[4] : book[constants.WEIGHT][index], 
+                constants.NEWHEADERS[5] : book[constants.AGE][index]
+                   }
+            writer.writerow(row)
+def main ():
+    fileReader = open(constants.PATHFITXERO, "r")
+    filecsv = csv.reader(fileReader,  delimiter = constants.SEPARADOROLD)
+    
+    book = toDictionarie(enumerate(filecsv))
+    book = changePosition(book)
+    book = changeHeightOrWeight(book, constants.HEIGHT)
+    book = changeHeightOrWeight(book, constants.WEIGHT)
+    book = changeAge(book, 0)
+    "showParameter(book, constants.HEIGHT)"
+    newCSVFile(book)
+main()
